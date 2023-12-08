3ceafb4e72eb258cbe22e02a1ceb99eef0a87e99
maxPaxAxe <124508506+maxPaxAxe@users.noreply.github.com>
2023-12-08 14:34:20 +0300

    Update ShoppingCartService.java

--- src/main/java/com/nphase/service/ShoppingCartService.java
+++ src/main/java/com/nphase/service/ShoppingCartService.java
@@ -1,17 +1,28 @@
 package com.nphase.service;
 
+import com.nphase.entity.Product;
 import com.nphase.entity.ShoppingCart;
 
 import javax.naming.OperationNotSupportedException;
 import java.math.BigDecimal;
 
 public class ShoppingCartService {
+    private static BigDecimal bigDecimal = new BigDecimal("0.9");
 
     public BigDecimal calculateTotalPrice(ShoppingCart shoppingCart) {
         return shoppingCart.getProducts()
                 .stream()
-                .map(product -> product.getPricePerUnit().multiply(BigDecimal.valueOf(product.getQuantity())))
+                .map(product ->)
                 .reduce(BigDecimal::add)
                 .orElse(BigDecimal.ZERO);
+    }
+
+    BigDecimal calculate(Product product) {
+
+        BigDecimal res = product.getPricePerUnit().multiply(BigDecimal.valueOf(product.getQuantity()));
+        if (product.getQuantity() >= 3) {
+            res = res.multiply(bigDecimal);
+        }
+        return res;
     }
 }
